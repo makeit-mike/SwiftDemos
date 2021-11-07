ef55d681dd724b7a43678b8ba0cb9674c8330a49
makeit-mike <mike.isaac.jones@gmail.com>
2021-11-07 12:47:47 -0600

    init

---
+++ SwiftDemos/SwiftDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemosApp.swift
+//  SwiftDemos
+//
+//  Created by Michael Jones on 11/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
